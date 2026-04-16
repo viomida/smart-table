544bc8c2c9012aa611bdc7947b767a547aff0aeb
AnnaKniga <catman696966@yandex.ru>
2026-04-16 13:33:51 +0300

    check it

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,14 @@
 import {rules, createComparison} from "../lib/compare.js";
 
 
-export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
-    }
+export function initSearching(searchField, fieldName = "search") {
+  return (query, state, action) => {
+    // result заменили на query
+    return state[fieldName]
+      ? Object.assign({}, query, {
+          // проверяем, что в поле поиска было что-то введено
+          search: state[fieldName], // устанавливаем в query параметр
+        })
+      : query; // если поле с поиском пустое, просто возвращаем query без изменений
+  };
 }
